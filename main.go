6c0ac5a95007f4bddddd90fecb1fad32bc53dba4
Shoshin Nikita <shoshin_nikita@fastmail.com>
2021-09-15 14:31:40 +0300

    start server, add graceful shutdown

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/ShoshinNikita/test-affise/internal/server"
+)
+
+func main() {
+	var (
+		serverPort = flag.Int("port", 8080, "server port")
+	)
+	flag.Parse()
+
+	// Start server
+	s := server.NewServer(server.Config{
+		Port: *serverPort,
+	})
+	errCh := make(chan error, 1)
+	go func() {
+		if err := s.Start(); err != nil {
+			errCh <- err
+		}
+	}()
+
+	termCh := make(chan os.Signal, 1)
+	signal.Notify(termCh, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-termCh:
+		log.Println("[INF] got an interrupt signal")
+	case err := <-errCh:
+		log.Printf("[ERR] server error, stop app: %s", err)
+	}
+
+	// Shutdown components
+	log.Println("[INF] stop app...")
+
+	if err := s.Stop(); err != nil {
+		log.Printf("[ERR] couldn't gracefully shutdown server: %s", err)
+	}
 }
