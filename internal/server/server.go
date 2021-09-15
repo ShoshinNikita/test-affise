aaf78cddbdb5a129d9a8c6ebaf716630109c32b0
Shoshin Nikita <shoshin_nikita@fastmail.com>
2021-09-15 14:31:13 +0300

    server: init `Server`

---
+++ internal/server/server.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	cfg        Config
+	httpServer *http.Server
+}
+
+type Config struct {
+	Port int
+}
+
+func NewServer(cfg Config) *Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleURLs)
+
+	return &Server{
+		cfg: cfg,
+		httpServer: &http.Server{
+			Addr:    fmt.Sprintf(":%d", cfg.Port),
+			Handler: mux,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	log.Printf("[DBG] start server on port :%d", s.cfg.Port)
+
+	if err := s.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) Stop() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	return s.httpServer.Shutdown(ctx)
+}
